4f207db9b3c1dcc83a4130f56d659441d01fc76f
Anushka Verma <v.anushka786@gmail.com>
2019-12-01 13:58:36 +0530

    add views

--- app.js
+++ app.js
@@ -3,26 +3,57 @@ var bodyParser       = require("body-parser"),
     methodOverride   = require("method-override"),
     expressSanitizer = require("express-sanitizer"),
     mongoose         = require("mongoose"),
+    path             = require("path"),
     express          = require("express"),
     app              = express();
 
 
 var db = require('./database.js');
+var Blog = require("./blogs.js");
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.use(expressSanitizer());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static("public"));
 app.set("view engine","ejs");
 
-var Blog = require("./blogs.js");
+Blog.create({
+    title: "b1",
+    image: "https://cdn.pixabay.com/photo/2015/06/01/09/04/blog-793047_960_720.jpg",
+    body: "b1 description"
+}, function(error, blog){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Blog Added");
+        
+    }
+});
 
+// ROUTES
+
+// HOMEPAGE
 app.get("/", function(req, res){
     res.send("blogs");
 });
 
+// INDEX ROUTE
+app.get("/blogs", function(req, res){
+
+    Blog.find({}, function(error, blogs){
+        if(error){
+            console.log("error");
+        }else{
+            console.log(blogs)
+            res.render("show.ejs", {blogs:blogs} );            
+        }
+    });
+
+});
 
 app.get("*", function(req, res){
-    res.send("DNE");
+    res.send("sorry DNE");
 });
 
 
